5f9282b1b5811d961057474c9b2a936052e24c83
Tchessi Pre <tchesspreza@yahoo.fr>
2021-10-02 12:40:55 +0200

    Mise en place de l'authentification

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const sauceRoutes = require("./routes/sauce");
+const userRoutes = require("./routes/user");
 mongoose
   .connect(
     "mongodb+srv://Tchessi_P6:LBBjudODJfgZl1Sv@cluster0.kl2up.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
@@ -11,22 +15,21 @@ mongoose
 const app = express();
 
 app.use((req, res, next) => {
-  console.log("Requête reçue !");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
+app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.json({ message: "Votre requête a bien été reçue !" });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log("Réponse envoyée avec succès !");
-});
+app.use("/api/sauces", sauceRoutes);
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
---
+++ backend/routes/sauce.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const sauceCtrl = require("../controllers/sauce");
+//const sauce = require("../models/sauce");
+const auth = require("../middleware/auth");
+
+router.post("/", auth, sauceCtrl.createSauce);
+router.put("/:id", auth, sauceCtrl.modifySauce);
+router.delete("/:id", auth, sauceCtrl.deleteSauce);
+router.get("/:id", auth, sauceCtrl.getOneSauce);
+router.use("/", auth, sauceCtrl.getAllSauces);
+
+module.exports = router;
